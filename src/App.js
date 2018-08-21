12a54543da40608d90d29ad9517a12ba478316b6
vractal <v@fractopia.org>
2018-08-21 15:07:25 -0300

    Create providers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { Provider as ReduxProvider } from "react-redux";
+import { Provider as CroodsProvider } from "croods";
 import { createReducer } from "croods";
-import { combineReducers, createStore, applyMiddleware, } from "redux";
+import { combineReducers, createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import "./App.css";
 
 const rootReducer = combineReducers({ beagle: createReducer("beagle") });
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
-
-
-export default props => <h1>Hello, Beagles!</h1>;
+export default props => (
+  <ReduxProvider store={store}>
+    <CroodsProvider baseUrl="https://dog.ceo/api/breed/beagle">
+      <h1>Hello, Beagles!</h1>
+    </CroodsProvider>
+  </ReduxProvider>
+);
