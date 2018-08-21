8b962d5d14d75d6d09806d303195714ac215968c
vractal <v@fractopia.org>
2018-08-21 15:05:35 -0300

    Create store

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { createReducer } from "croods";
-import { combineReducers } from "redux";
+import { combineReducers, createStore, applyMiddleware, } from "redux";
+import thunk from "redux-thunk";
 import "./App.css";
 
 const rootReducer = combineReducers({ beagle: createReducer("beagle") });
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+
 
 export default props => <h1>Hello, Beagles!</h1>;
