367ebb5ac156080d3f84cfc169a7e72a1b4652a3
vractal <v@fractopia.org>
2018-08-21 15:12:01 -0300

    Create List

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Provider as ReduxProvider } from "react-redux";
 import { Provider as CroodsProvider } from "croods";
-import { createReducer } from "croods";
+import { createReducer, List } from "croods";
 import { combineReducers, createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import "./App.css";
@@ -11,8 +11,28 @@ const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default props => (
   <ReduxProvider store={store}>
-    <CroodsProvider baseUrl="https://dog.ceo/api/breed/beagle">
-      <h1>Hello, Beagles!</h1>
+    <CroodsProvider
+      baseUrl="https://dog.ceo/api"
+      parseListResponse={({ message: list }) => ({ list })}
+    >
+      <List
+        name="beagle"
+        path="/breed/beagle/images"
+        render={list => (
+          <div>
+            <h1>Hello, Beagles!</h1>
+            <ul>
+              {list.map((item, index) => {
+                return index < 10 ? (
+                  <li key={index}>
+                    <img src={item} alt="" />
+                  </li>
+                ) : null;
+              })}
+            </ul>
+          </div>
+        )}
+      />
     </CroodsProvider>
   </ReduxProvider>
 );
